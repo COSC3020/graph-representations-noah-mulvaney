161d804ddf050e401d55ce619e733ddac52b695c
Noah Mulvaney <127539141+noah-mulvaney@users.noreply.github.com>
2024-02-28 16:09:08 -0700

    Add list to matrix conversion

--- code.js
+++ code.js
@@ -2,6 +2,7 @@
 // Noah Mulvaney, nmulvane@uwyo.edu
 // 28 Feb 2024
 
+// matrix to list
 function convertToAdjList(adj_matrix) {
     let adj_list = [];
     
@@ -17,19 +18,60 @@ function convertToAdjList(adj_matrix) {
     return adj_list;
 }
 
-function my_test(am) {
-    console.log("Input: ");
+// test matrix to list
+function test_to_list(am) {
+    console.log("Matrix");
     for (let i = 0; i < am.length; i++) {
         console.log(am[i]);
     }
     
     al = convertToAdjList(am);
-    console.log("Output:");
+    console.log("List:");
     for (let i = 0; i < al.length; i++) {
         console.log(al[i]);
     }
 }
 
-my_test([[0, 1], [1, 1]]);
-my_test([[0, 1, 0], [0, 0, 0], [1, 1, 0]]);
-my_test([[0, 1, 0, 1], [0, 0, 0, 1], [1, 1, 0, 1], [0, 1, 1, 1]]);
+test_to_list([[0, 1], [1, 1]]);
+test_to_list([[0, 1, 0], [0, 0, 0], [1, 1, 0]]);
+test_to_list([[0, 1, 0, 1], [0, 0, 0, 1], [1, 1, 0, 1], [0, 1, 1, 1]]);
+
+// list to matrix
+function convert_to_matrix(adj_list) {
+    let matrix = [];
+    let v = adj_list.length;
+    
+    // create matrix of correct size and all zeros
+    for (let i = 0; i < v; i++) {
+        let row = [];
+        for (let j = 0; j < v; j++) {
+            row[j] = 0;
+        }
+        matrix.push(row);
+    }
+
+    // set appropriate entries to one
+    for (let i = 0; i < adj_list.length; i++) 
+        for (let j = 0; j < adj_list[i].length; j++)
+            matrix[i][adj_list[i][j]] = 1;
+            
+    return matrix;
+}
+
+// test list to matrix
+function test_to_matrix(al) {
+    console.log("List");
+    for (let i = 0; i < al.length; i++) {
+        console.log(al[i]);
+    }
+    
+    am = convert_to_matrix(al);
+    console.log("Matrix:");
+    for (let i = 0; i < al.length; i++) {
+        console.log(am[i]);
+    }
+}
+
+test_to_matrix([[1], [0, 1]]);
+test_to_matrix([[1], [0, 1], [0, 1, 2]]);
+test_to_matrix([[1, 3], [0, 1], [1, 2], []]);
